de67ecc08410fc9569df39f3dd60a1f2e8b66e6b
jinsupark4255 <jin04007@naver.com>
2023-04-28 20:01:05 +0900

    final

--- node.js
+++ node.js
@@ -1,19 +1,50 @@
-const input = document.querySelector(".app-insert input");//input 박스
-const toDos = document.querySelector(".app-list");//만들어지는 박스
+const input = document.querySelector(".app-insert input");
+const toDos = document.querySelector(".app-list");
 
-function addTask(task){
-    let newTask = document.createElement("li");
-    newTask.setAttribute("class","task");
-    newTask.innerHTML = task + '<a href="javascript:;" class="remove-task">remove</a>' 
-    let list= document.querySelector(".app-list ul");
-    list.appendChild(newTask);
+// Add task
+input.addEventListener(  // input 요소에 이벤트 리스너 추가
+  "keyup",  // 키보드 입력
+  function (event) {  // 이벤트 리스너
+    if (event.keyCode === 13) {  // 엔터 입력
+      if (input.value !== "") {  // 입력값이 있을 때만
+        addTask(input.value);  // 입력값 추가
+        input.value = "";  // 입력값 초기화
+      }
+    }
+  },
+  false  // 이벤트 버블링 방지
+);
+
+toDos.addEventListener(  // toDos 요소에 이벤트 리스너 추가
+  "click",  // 마우스 클릭
+  function (event) {
+    if (event.target.classList.contains("remove-task")) {  // remove-task 클래스를 가지고 있을 때
+      removeTask(event.target.parentNode);  // 부모 노드 삭제
+
+      // Complete Task
+    } else if (event.target.classList.contains("task")) {  // task 클래스를 가지고 있을 때
+      completeTask(event.target);  // task 클래스 토글
+    }
+  },
+  false  // 이벤트 버블링 방지
+);
+
+function addTask(task) {  // task 추가
+  var newTask = document.createElement("li");  // li 요소 생성
+  newTask.setAttribute("class", "task");  // li 요소에 class 속성 추가
+  newTask.innerHTML = task + '<a href="javascript:;" class="remove-task">remove</a>';  // li 요소에 내용 추가
+
+  var list = document.querySelector(".app-list ul");  // ul 요소 선택
+  list.appendChild(newTask);  // ul 요소에 li 요소 추가
 }
 
-input.addEventListener("keyup",function(event) {
-    if(event.keyCode === 13){
-        if(input.value !== ""){
-            addTask(input.value);
-            input.value="";
-        }
-    }
-},false);
+function removeTask(task) {  // task 삭제
+  task.style.opacity = 0;  // 투명도 0으로 설정
+  setTimeout(function () {  // 0.4초 후에
+    task.remove();  // task 삭제
+  }, 400);
+}
+
+function completeTask(task) {  // task 완료
+  task.classList.toggle("task-complete");  // task-complete 클래스 토글
+}
